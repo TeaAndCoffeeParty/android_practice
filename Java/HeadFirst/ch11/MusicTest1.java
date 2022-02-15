9493384608a303e78a9985c530e05e04bdbf50f7
chentuo <chentuo@soonsolid.com>
2022-02-15 10:37:49 +0800

    add try/catch

--- Java/HeadFirst/ch11/MusicTest1.java
+++ Java/HeadFirst/ch11/MusicTest1.java
@@ -2,8 +2,12 @@ import javax.sound.midi.*;
 
 public class MusicTest1 {
 	public void play() {
-		Sequencer sequencer = MidiSystem.getSequencer();
-		System.out.println("We got a sequencer");
+		try {
+			Sequencer sequencer = MidiSystem.getSequencer();
+			System.out.println("Successfully got a sequencer");
+		} catch(MidiUnavailableException ex) {
+			System.out.println("Bummer");
+		}
 	}
 
 	public static void main(String[] args) {
