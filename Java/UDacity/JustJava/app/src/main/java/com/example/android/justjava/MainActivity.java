51d128e61fb952cbd05f3f8e77a31a066a67f84d
chentuo <chentuo@soonsolid.com>
2022-02-22 11:28:03 +0800

    add a price view

--- Java/UDacity/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Java/UDacity/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -9,11 +9,13 @@
 package com.example.android.justjava;
 
 
-
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -30,7 +32,16 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(countNumber++);
+        display(2);
+        displayPrice(2*5);
+    }
+
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
     /**
