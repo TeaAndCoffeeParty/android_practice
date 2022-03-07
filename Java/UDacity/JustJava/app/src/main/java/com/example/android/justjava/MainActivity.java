415a4d9fc881325653e50069c32800ab0d7308dd
chentuo <chentuo@soonsolid.com>
2022-03-07 19:25:51 +0800

    add a price action

--- Java/UDacity/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Java/UDacity/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -27,22 +27,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    int quantity = 2;
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int quantity = 2;
-        display(quantity);
-        displayPrice(quantity*5);
+//        display(quantity);
+//        displayPrice(quantity*5);
+        String priceMessage = "$"+quantity*5;
+        displayMessage(priceMessage);
     }
 
     public void increment(View view) {
-        int quantity = 3;
+        quantity++;
         display(quantity);
     }
 
     public void decrement(View view) {
-        int quantity = 1;
+        quantity--;
         display(quantity);
     }
 
@@ -60,5 +62,13 @@ public class MainActivity extends AppCompatActivity {
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
