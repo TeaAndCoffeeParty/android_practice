7e25c735807c15aa0a21a2b50fba538a2bba3763
chentuo <chentuo@soonsolid.com>
2022-04-25 15:13:06 +0800

    add a price action and '-' '+'

--- Java/UDacity/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Java/UDacity/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -39,12 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void increment(View view) {
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
         quantity--;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     /**
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
