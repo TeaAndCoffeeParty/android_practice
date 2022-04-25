ff177f81abb756f7d282d406cab6024b87a902a3
chentuo <chentuo@soonsolid.com>
2022-04-25 19:09:27 +0800

    create a summary function

--- Java/UDacity/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Java/UDacity/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
@@ -66,8 +66,8 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     private int calculatePrice() {
