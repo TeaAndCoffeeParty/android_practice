1fe7c4b55ac97368c8ae58e1dcf47b1ba324886f
chentuo <chentuo@soonsolid.com>
2022-12-01 13:31:53 +0800

    roll dice application by ImageView

--- kotlin/android-training-get-started/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ kotlin/android-training-get-started/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -18,7 +19,15 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage = findViewById<ImageView>(R.id.dice_image)
+        val drawableResource = when(randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
     }
 }
