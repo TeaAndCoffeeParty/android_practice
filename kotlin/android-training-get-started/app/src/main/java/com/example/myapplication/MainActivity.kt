873f04571872386b228dd0d8edb3564125bc2162
chentuo <chentuo@soonsolid.com>
2022-12-05 17:30:31 +0800

    add image view

--- kotlin/android-training-get-started/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ kotlin/android-training-get-started/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -9,18 +10,39 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     lateinit var diceImage: ImageView
+    lateinit var diceImage1: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
         diceImage = findViewById<ImageView>(R.id.dice_image)
+        diceImage1 = findViewById<ImageView>(R.id.dice_image1)
         val rollButton: Button = findViewById<Button>(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        findViewById<Button>(R.id.clear_button).setOnClickListener { cleanDicImage() }
+
     }
 
     private fun rollDice() {
         Toast.makeText(this, "button clicked",
             Toast.LENGTH_SHORT).show()
-        val drawableResource = when((1..6).random()) {
+//        val drawableResource = when((1..6).random()) {
+//            1 -> R.drawable.dice_1
+//            2 -> R.drawable.dice_2
+//            3 -> R.drawable.dice_3
+//            4 -> R.drawable.dice_4
+//            5 -> R.drawable.dice_5
+//            else -> R.drawable.dice_6
+//        }
+//        diceImage?.setImageResource(drawableResource)
+        diceImage.setImageResource(getRandomDiceImage())
+        diceImage1.setImageResource(getRandomDiceImage())
+    }
+
+    private fun getRandomDiceImage() : Int {
+        return  when((1..6).random()) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
             3 -> R.drawable.dice_3
@@ -28,6 +50,10 @@ class MainActivity : AppCompatActivity() {
             5 -> R.drawable.dice_5
             else -> R.drawable.dice_6
         }
-        diceImage?.setImageResource(drawableResource)
+    }
+
+    private fun cleanDicImage() {
+        diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage1.setImageResource(R.drawable.empty_dice)
     }
 }
