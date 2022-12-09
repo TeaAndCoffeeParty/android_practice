ccb83c32b78f9f7fa5ed373ea354186349ff704e
chentuo <chentuo@soonsolid.com>
2022-12-09 17:22:22 +0800

    add play button in fragment_about.xml and fragment_rules.xml

--- kotlin/06-android-training-create-and-add-fragment/app/src/main/java/com/example/android/navigation/AboutFragment.kt
+++ kotlin/06-android-training-create-and-add-fragment/app/src/main/java/com/example/android/navigation/AboutFragment.kt
@@ -21,11 +21,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.android.navigation.databinding.FragmentAboutBinding
 
 class AboutFragment : Fragment() {
+    private lateinit var binding: FragmentAboutBinding
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
+        binding = DataBindingUtil.inflate<FragmentAboutBinding>(inflater,
+            R.layout.fragment_about, container, false)
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_aboutFragment_to_gameFragment)
+        }
+
+        return binding.root
     }
 }
