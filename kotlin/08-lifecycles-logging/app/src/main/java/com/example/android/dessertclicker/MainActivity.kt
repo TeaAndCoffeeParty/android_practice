1e6dc4463a8a490d0c31a1a6b680d95fe8fa37c2
chentuo <chentuo@soonsolid.com>
2022-12-13 16:55:55 +0800

    add timber log and test

--- kotlin/08-lifecycles-logging/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ kotlin/08-lifecycles-logging/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -79,12 +80,12 @@ class MainActivity : AppCompatActivity() {
         // Make sure the correct dessert is showing
         binding.dessertButton.setImageResource(currentDessert.imageId)
 
-        Log.i("MainActivity", "onCreate Called")
+        Timber.i("onCreate Called")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.i("MainActivity", "onStart Called")
+        Timber.i("onStart Called")
     }
 
     /**
@@ -152,5 +153,30 @@ class MainActivity : AppCompatActivity() {
             R.id.shareMenuButton -> onShare()
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
     }
 }
