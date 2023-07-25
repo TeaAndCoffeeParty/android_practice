996bd0a88bd7e08678234de1616b7a9e4e0a5c54
chentuo <chentuo@soonsolid.com>
2023-07-25 13:51:48 +0800

    add EditText and search button

--- kotlin/MyWeather/app/src/main/java/com/example/myweather/MainActivity.kt
+++ kotlin/MyWeather/app/src/main/java/com/example/myweather/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.myweather
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        RetrofitClient.getWeatherByCityName("London")
+        findViewById<Button>(R.id.buttonSearch).setOnClickListener { searchCityNameWeather(it) }
+    }
 
+    private fun searchCityNameWeather(view: View) {
+        var cityName = findViewById<EditText>(R.id.editTextCity).text.toString().trim()
+        RetrofitClient.getWeatherByCityName(cityName)
     }
 }
