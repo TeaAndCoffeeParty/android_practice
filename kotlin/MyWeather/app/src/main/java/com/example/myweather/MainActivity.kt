e346c205cee74941147b89587303f0b738701c41
chentuo <chentuo@soonsolid.com>
2023-07-25 15:58:51 +0800

    show the result in TextView

--- kotlin/MyWeather/app/src/main/java/com/example/myweather/MainActivity.kt
+++ kotlin/MyWeather/app/src/main/java/com/example/myweather/MainActivity.kt
@@ -1,21 +1,38 @@
 package com.example.myweather
 
+import android.content.IntentFilter
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var weatherResponseReceiver: WeatherResponseReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        weatherResponseReceiver = WeatherResponseReceiver()
+        weatherResponseReceiver.setTextView(findViewById<TextView>(R.id.weatherResult))
+        val intentFilter = IntentFilter("com.example.MyWeather.ACTION_WEATHER_DATA")
+        registerReceiver(weatherResponseReceiver, intentFilter)
+
         findViewById<Button>(R.id.buttonSearch).setOnClickListener { searchCityNameWeather(it) }
     }
 
-    private fun searchCityNameWeather(view: View) {
-        var cityName = findViewById<EditText>(R.id.editTextCity).text.toString().trim()
-        RetrofitClient.getWeatherByCityName(cityName)
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(weatherResponseReceiver)
     }
+
+    private fun searchCityNameWeather(view: View) {
+        val cityName = findViewById<EditText>(R.id.editTextCity).text.toString().trim()
+        RetrofitClient.getWeatherByCityName(view.context, cityName)
+    }
+
+
+
 }
--- kotlin/MyWeather/app/src/main/java/com/example/myweather/RetrofitClient.kt
+++ kotlin/MyWeather/app/src/main/java/com/example/myweather/RetrofitClient.kt
@@ -1,5 +1,7 @@
 package com.example.myweather
 
+import android.content.Context
+import android.content.Intent
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -13,11 +15,11 @@ object RetrofitClient {
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .build();
+        .build()
 
     private val weatherService: WeatherService by lazy { retrofit.create(WeatherService::class.java) }
 
-    fun getWeatherByCityName(cityName: String) {
+    fun getWeatherByCityName(context: Context, cityName: String) {
         val call = weatherService.getWeatherByCityName(cityName, API_KEY)
         call.enqueue(object : Callback<WeatherResponse> {
             override fun onResponse(
@@ -26,7 +28,7 @@ object RetrofitClient {
             ) {
                 if (response.isSuccessful) {
                     val weatherData = response.body()
-                    handleWeatherData(weatherData)
+                    handleWeatherData(context, weatherData)
                 } else {
                     handleWeatherFailure(response.message())
                 }
@@ -38,23 +40,47 @@ object RetrofitClient {
         })
     }
 
-    private fun handleWeatherData(weatherData: WeatherResponse?) {
+    private fun handleWeatherData(context: Context, weatherData: WeatherResponse?) {
         if (weatherData != null) {
-            println("coord: lat:${weatherData.coord?.lat},lon:${weatherData.coord?.lon}")
+            val intent = Intent("com.example.MyWeather.ACTION_WEATHER_DATA")
+            intent.putExtra("cityName", weatherData.name)
+            intent.putExtra("temperature", weatherData.main?.temp)
+            intent.putExtra("maxTemperature", weatherData.main?.temp_max)
+            intent.putExtra("minTemperature", weatherData.main?.temp_min)
+            val weatherStringArray = arrayListOf<String>()
             for(weather in weatherData.weather) {
-                println("weather: id:${weather.id},main${weather.main}," +
-                        "description:${weather.description},icon:${weather.icon}")
+                weatherStringArray += "main:${weather.main},description:${weather.description}"
+            }
+            intent.putStringArrayListExtra("weather", weatherStringArray)
+            context.sendBroadcast(intent)
+
+            printWeatherData(weatherData)
+        }
+    }
+
+    private fun printWeatherData(weatherData: WeatherResponse?) {
+        if (weatherData != null) {
+            println("cord: lat:${weatherData.coord?.lat},lon:${weatherData.coord?.lon}")
+            for (weather in weatherData.weather) {
+                println(
+                    "weather: id:${weather.id},main:${weather.main}," +
+                            "description:${weather.description},icon:${weather.icon}"
+                )
             }
             println("base:${weatherData.base}")
-            println("main: temperature:${weatherData.main?.temp},pressure:${weatherData.main?.pressure}," +
-                    "humidity:${weatherData.main?.humidity},temperature_min:${weatherData.main?.temp_min}," +
-                    "temperature_max:${weatherData.main?.temp_max}")
+            println(
+                "main: temperature:${weatherData.main?.temp},pressure:${weatherData.main?.pressure}," +
+                        "humidity:${weatherData.main?.humidity},temperature_min:${weatherData.main?.temp_min}," +
+                        "temperature_max:${weatherData.main?.temp_max}"
+            )
             println("visibility:${weatherData.visibility}")
             println("wind: speed:${weatherData.wind?.speed},deq:${weatherData.wind?.deg}")
             println("clouds: clouds:${weatherData.clouds?.clouds}")
             println("dt: ${weatherData.dt}")
-            println("sys: type:${weatherData.sys?.type},id:${weatherData.sys?.id},message:${weatherData.sys?.message}" +
-                    ",country:${weatherData.sys?.country},+sunrise:${weatherData.sys?.sunrise},+sunset:${weatherData.sys?.sunset}")
+            println(
+                "sys: type:${weatherData.sys?.type},id:${weatherData.sys?.id},message:${weatherData.sys?.message}" +
+                        ",country:${weatherData.sys?.country},+sunrise:${weatherData.sys?.sunrise},+sunset:${weatherData.sys?.sunset}"
+            )
             println("id: ${weatherData.id}")
             println("name: ${weatherData.name}")
             println("cod: ${weatherData.cod}")
