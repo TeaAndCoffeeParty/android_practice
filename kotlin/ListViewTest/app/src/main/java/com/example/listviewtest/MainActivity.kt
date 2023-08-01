a62c4bbd165b6fc5f00a4b897324ad963460af00
chentuo <chentuo@soonsolid.com>
2023-08-01 16:20:06 +0800

    first commit

---
+++ kotlin/ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.listviewtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    private val data = listOf("Apple", "Banana", "Orange", "Watermelon",
+        "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango",
+        "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape",
+        "Pineapple", "Strawberry", "Cherry", "Mango")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        findViewById<ListView>(R.id.listView).adapter = adapter
+    }
+}
