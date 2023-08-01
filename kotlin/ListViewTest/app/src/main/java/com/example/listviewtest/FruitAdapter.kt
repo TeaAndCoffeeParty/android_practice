c43e73d313f049e6de20010ee83ddf82350b90a0
chentuo <chentuo@soonsolid.com>
2023-08-01 17:08:01 +0800

    improve the performance

--- kotlin/ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.kt
+++ kotlin/ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.kt
@@ -8,17 +8,28 @@ import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
 
-class FruitAdapter (activity: Activity, val resourceId: Int, data: List<Fruit>):
+class FruitAdapter (activity: Activity, private val resourceId: Int, data: List<Fruit>):
     ArrayAdapter<Fruit>(activity, resourceId, data) {
 
+    inner class ViewHolder(val fruitImage: ImageView, val fruitName: TextView)
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
-        val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
-        val fruitName: TextView = view.findViewById(R.id.fruitName)
+        val view: View
+        val viewHolder: ViewHolder
+        if(convertView == null) {
+            view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+            val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
+            val fruitName: TextView = view.findViewById(R.id.fruitName)
+            viewHolder = ViewHolder(fruitImage, fruitName)
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = view.tag as ViewHolder
+        }
         val fruit = getItem(position)
         if(fruit != null) {
-            fruitImage.setImageResource(fruit.imageId)
-            fruitName.text = fruit.name
+            viewHolder.fruitImage.setImageResource(fruit.imageId)
+            viewHolder.fruitName.text = fruit.name
         }
         return view
     }
