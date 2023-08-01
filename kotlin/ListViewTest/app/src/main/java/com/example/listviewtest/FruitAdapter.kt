6f90f756894ed4837d011969b9b094a90cea957d
chentuo <chentuo@soonsolid.com>
2023-08-01 16:50:37 +0800

    add FruitAdapter

---
+++ kotlin/ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.listviewtest
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+
+class FruitAdapter (activity: Activity, val resourceId: Int, data: List<Fruit>):
+    ArrayAdapter<Fruit>(activity, resourceId, data) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = LayoutInflater.from(context).inflate(resourceId, parent, false)
+        val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
+        val fruitName: TextView = view.findViewById(R.id.fruitName)
+        val fruit = getItem(position)
+        if(fruit != null) {
+            fruitImage.setImageResource(fruit.imageId)
+            fruitName.text = fruit.name
+        }
+        return view
+    }
+}
--- kotlin/ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.kt
+++ kotlin/ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.kt
@@ -10,11 +10,28 @@ class MainActivity : AppCompatActivity() {
         "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango",
         "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape",
         "Pineapple", "Strawberry", "Cherry", "Mango")
+    private val fruitList = ArrayList<Fruit>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        initFruits()
+        val adapter = FruitAdapter(this, R.layout.fruit_item, fruitList)
         findViewById<ListView>(R.id.listView).adapter = adapter
+    }
+
+    private fun initFruits() {
+        repeat(2) {
+            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
+            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
+            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
+            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
+            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
+            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
+            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
+            fruitList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
+            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
+            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+        }
     }
 }
