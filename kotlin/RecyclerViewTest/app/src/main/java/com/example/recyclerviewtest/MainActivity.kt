ab88cb10a5151a96b6c59b180fd1f98c9f8f3906
chentuo <chentuo@soonsolid.com>
2023-08-01 18:56:31 +0800

    recycleListView example

---
+++ kotlin/RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.recyclerviewtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.listviewtest.Fruit
+import com.example.listviewtest.FruitAdapter
+
+class MainActivity : AppCompatActivity() {
+    private val fruitList = ArrayList<Fruit>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initFruits()
+        val layoutManager = LinearLayoutManager(this)
+        val recyclerView  = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = layoutManager
+        val adapter = FruitAdapter(fruitList)
+        recyclerView.adapter = adapter
+    }
+
+    private fun initFruits() {
+        repeat(2) {
+            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
+            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
+            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
+            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
+            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
+            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
+            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
+            fruitList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
+            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
+            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+        }
+    }
+}
