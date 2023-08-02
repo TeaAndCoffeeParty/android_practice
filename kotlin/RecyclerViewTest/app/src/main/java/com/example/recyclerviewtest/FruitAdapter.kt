3bdddce3ccda094ec15e9e85b03afd751faa5657
chentuo <chentuo@soonsolid.com>
2023-08-02 17:05:27 +0800

    change to staggerGridLayoutManager

--- kotlin/RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.kt
+++ kotlin/RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/FruitAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewtest.R
 
@@ -18,6 +19,7 @@ class FruitAdapter(private val fruitList: List<Fruit>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.fruit_item, parent, false)
+        val viewHolder = ViewHolder(view)
         return ViewHolder(view)
     }
 
@@ -25,6 +27,20 @@ class FruitAdapter(private val fruitList: List<Fruit>) :
         val fruit = fruitList[position]
         holder.fruitImage.setImageResource(fruit.imageId)
         holder.fruitName.text = fruit.name
+        holder.itemView.setOnClickListener {
+            val position = holder.bindingAdapterPosition
+            println("position:${position}")
+            val fruit = fruitList[position]
+            Toast.makeText(holder.itemView.context, "you clicked view ${fruit.name}",
+                Toast.LENGTH_SHORT).show()
+        }
+        holder.fruitImage.setOnClickListener {
+            val position = holder.bindingAdapterPosition
+            println("position:${position}")
+            val fruit = fruitList[position]
+            Toast.makeText(holder.itemView.context, "you clicked image ${fruit.name}",
+                Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun getItemCount() = fruitList.size
--- kotlin/RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
+++ kotlin/RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.listviewtest.Fruit
 import com.example.listviewtest.FruitAdapter
 
@@ -14,7 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initFruits()
-        val layoutManager = LinearLayoutManager(this)
+        val layoutManager = StaggeredGridLayoutManager(3,
+            StaggeredGridLayoutManager.VERTICAL)
+//        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
         val recyclerView  = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.layoutManager = layoutManager
         val adapter = FruitAdapter(fruitList)
@@ -23,16 +26,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun initFruits() {
         repeat(2) {
-            fruitList.add(Fruit("Apple", R.drawable.apple_pic))
-            fruitList.add(Fruit("Banana", R.drawable.banana_pic))
-            fruitList.add(Fruit("Orange", R.drawable.orange_pic))
-            fruitList.add(Fruit("Watermelon", R.drawable.watermelon_pic))
-            fruitList.add(Fruit("Pear", R.drawable.pear_pic))
-            fruitList.add(Fruit("Grape", R.drawable.grape_pic))
-            fruitList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
-            fruitList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
-            fruitList.add(Fruit("Cherry", R.drawable.cherry_pic))
-            fruitList.add(Fruit("Mango", R.drawable.mango_pic))
+            fruitList.add(Fruit(getRandomLengthString("Apple"), R.drawable.apple_pic))
+            fruitList.add(Fruit(getRandomLengthString("Orange"), R.drawable.orange_pic))
+            fruitList.add(Fruit(getRandomLengthString("Banana"), R.drawable.banana_pic))
+            fruitList.add(Fruit(getRandomLengthString("Watermelon"), R.drawable.watermelon_pic))
+            fruitList.add(Fruit(getRandomLengthString("Pear"), R.drawable.pear_pic))
+            fruitList.add(Fruit(getRandomLengthString("Grape"), R.drawable.grape_pic))
+            fruitList.add(Fruit(getRandomLengthString("Pineapple"), R.drawable.pineapple_pic))
+            fruitList.add(Fruit(getRandomLengthString("Strawberry"), R.drawable.strawberry_pic))
+            fruitList.add(Fruit(getRandomLengthString("Cherry"), R.drawable.cherry_pic))
+            fruitList.add(Fruit(getRandomLengthString("Mango"), R.drawable.mango_pic))
         }
+    }
+
+    private fun getRandomLengthString(str: String) : String {
+        val n = (1..20).random()
+        val builder = java.lang.StringBuilder()
+        repeat(n) {
+            builder.append(str)
+        }
+        return builder.toString()
     }
 }
