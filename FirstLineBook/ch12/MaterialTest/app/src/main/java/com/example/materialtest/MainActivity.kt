d1c4acb6fcbf400dc9aebf30c172bad11401002c
chentuo <chentuo@soonsolid.com>
2024-04-12 15:33:50 +0800

    add binding

--- FirstLineBook/ch12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.kt
+++ FirstLineBook/ch12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.materialtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.materialtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
     }
 }
