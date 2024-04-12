d51241dde601677227d93c1c1a65c69f33e72fb0
chentuo <chentuo@soonsolid.com>
2024-04-12 16:01:02 +0800

    add toolbar action

--- FirstLineBook/ch12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.kt
+++ FirstLineBook/ch12/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.materialtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.example.materialtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,22 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.backup -> Toast.makeText(this, "You clicked Backup",
+                Toast.LENGTH_SHORT).show()
+            R.id.delete -> Toast.makeText(this, "You clicked Delete",
+                Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this, "You clicked Settings",
+                Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
