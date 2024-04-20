827b7ab72fa1a321acc71f0951f57a31a254aed4
Chen Tuo <chentuo@soonsolid.com>
2024-04-20 22:10:07 +0800

    add gion and read file content

--- kotlin/MyWeather/app/src/main/java/com/example/myweather/cityListUtils/CityListDataManager.kt
+++ kotlin/MyWeather/app/src/main/java/com/example/myweather/cityListUtils/CityListDataManager.kt
@@ -2,13 +2,19 @@ package com.example.myweather.cityListUtils
 
 import android.content.Context
 import android.util.Log
+import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileInputStream
 import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
 import java.util.zip.GZIPInputStream
+
 
 class CityListDataManager(private val context: Context) {
     private val tag = "CityListDataManager"
@@ -19,6 +25,8 @@ class CityListDataManager(private val context: Context) {
             CoroutineScope(Dispatchers.IO).launch {
                 unzipGzFile(context)
             }
+        } else {
+            testGetJsonData(context)
         }
     }
 
@@ -42,6 +50,23 @@ class CityListDataManager(private val context: Context) {
             } catch (ex: Exception) {
                 ex.printStackTrace()
             }
+        }
+    }
+
+    private fun testGetJsonData(context: Context) {
+        var file = File(context.filesDir, jsonFileName)
+        try {
+            val fis = FileInputStream(file)
+            val reader = BufferedReader(InputStreamReader(fis))
+            val stringBuilder = StringBuilder()
+            var line : String?
+            while (reader.readLine().also { line = it } != null) {
+                stringBuilder.append(line).append("\n")
+                Log.d(tag, "line:$line")
+            }
+            val fileContent = stringBuilder.toString()
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 }
