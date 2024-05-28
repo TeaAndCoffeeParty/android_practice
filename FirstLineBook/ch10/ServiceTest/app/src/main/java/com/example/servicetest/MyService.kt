e6f5e80ec11dd91688ed7014f27c0af1cb6ff607
chentuo <chentuo@soonsolid.com>
2024-05-28 12:00:50 +0800

    add fore ground service

--- FirstLineBook/ch10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.kt
+++ FirstLineBook/ch10/ServiceTest/app/src/main/java/com/example/servicetest/MyService.kt
@@ -1,10 +1,17 @@
 package com.example.servicetest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.core.app.NotificationCompat
 
 class MyService : Service() {
     val TAG = javaClass.simpleName
@@ -24,6 +31,22 @@ class MyService : Service() {
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "onCreate executed")
+        val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel("my_service", "前台Service通知",
+                NotificationManager.IMPORTANCE_DEFAULT)
+            manager.createNotificationChannel(channel)
+        }
+        val intent = Intent(this, MainActivity::class.java)
+        val pi = PendingIntent.getActivity(this, 0, intent, 0)
+        val notification = NotificationCompat.Builder(this, "my_service")
+            .setContentTitle("This is content title")
+            .setContentText("This is content text")
+            .setSmallIcon(R.drawable.small_icon)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
+            .setContentIntent(pi)
+            .build()
+        startForeground(1, notification)
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
